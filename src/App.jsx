323857731dcee90d5d8c08a3b5a871020c78f2ec
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-07-04 16:43:54 +0200

    Update Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-    {/* <NavBar /> */}
+    <NavBar />
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path= '/products' element={<Products/>}/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,18 @@
+import { PiShoppingCart } from 'react-icons/pi';
+import pambaNaPerle from '../assets/pamba-na-perle.png'
+
 const NavBar = () => {
   return (   
-<nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" className="flex items-center">
-      <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"/>
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+<nav className="fixed bg-slate-900 px-8 z-20 top-0 left-0 right-0 h-[70px] flex justify-between items-center">
+  <a href="/" className="h-[65px] w-[65px]">
+    <img src={pambaNaPerle} alt="Logo" className="h-full w-full object-contain" />
   </a>
-  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
-      </li>
-      <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-      </li>
-      <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-      </li>
-      <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-      </li>
-    </ul>
-  </div>
-  </div>
+    <form className="w-[50%]">
+      <input type="text" className="bg-white h-[2.5rem] border border-gray-300 w-full" />
+    </form>
+    <button className='text-2xl text-white'>
+      <PiShoppingCart />
+    </button>
 </nav>   
   )
 }
